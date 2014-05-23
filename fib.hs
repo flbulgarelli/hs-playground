f22652d3870b074858b8f94a20de3130ca1b1b27
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-05-23 12:28:12 -0300

    Using until instead of while

--- fib.hs
+++ fib.hs
@@ -18,11 +18,8 @@ fib''' n  =  f 1 1 !! n
 
 f x0 x1 = x0 : f x1 (x0 + x1)
 
-
-while c a i = if (c a) then while c (i a) i else a
-
 fib'''' n = case n of
               0 -> 1
               1 -> 1
-              _ -> let (_,r,_) = while (\(_, _, i) -> i <= n-2) (1, 1, 0) (\(x0, x1, i) -> (x1, x0 + x1, i + 1)) 
+              _ -> let (_,r,_) = until (\(_, _, i) -> i > n-2) (\(x0, x1, i) -> (x1, x0 + x1, i + 1)) (1, 1, 0) 
                    in r
